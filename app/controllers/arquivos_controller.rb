15644ff1b1cd7248f7ae208d0559160a068bff14
Felipe <fdeodato11@gmail.com>
2019-11-06 01:34:32 -0200

    Layout basico
    Signed-off-by: Felipe <fdeodato11@gmail.com>

--- app/controllers/arquivos_controller.rb
+++ app/controllers/arquivos_controller.rb
@@ -1,5 +1,6 @@
 class ArquivosController < ApplicationController
-
+  before_action :set_arquivo, only: [:show, :edit, :update, :destroy]
+  
   def index
     @arquivos = current_user.arquivos
   end
@@ -12,6 +13,18 @@ class ArquivosController < ApplicationController
   def edit
   end
 
+  def update
+    respond_to do |format|
+      if @arquivo.update(arquivo_params)
+        format.html { redirect_to @arquivo, notice: 'arquivo was successfully updated.' }
+        format.json { render :show, status: :ok, location: @arquivo }
+      else
+        format.html { render :edit }
+        format.json { render json: @arquivo.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   # POST /arquivos
   # POST /arquivos.json
   def create
@@ -22,7 +35,6 @@ class ArquivosController < ApplicationController
         ligacao.arquivo_id = @arquivo.id
         ligacao.user_id = current_user.id
         ligacao.save! 
-        byebug
         format.html { redirect_to arquivos_url, notice: 'Arquivo was successfully created.' }
         format.json { render :idex, status: :created, location: @arquivo }
       else
@@ -36,6 +48,10 @@ class ArquivosController < ApplicationController
   end
 
   private 
+
+  def set_arquivo
+    @arquivo = Arquivo.find(params[:id])
+  end
 
   def arquivo_params
     params.require(:arquivo).permit(:nome)
