0cd904dc548d9103f27d7faaaf0c082225664255
Felipe <fdeodato11@gmail.com>
2019-11-06 00:14:47 -0200

    Criacao de arquivo lincado ao usuario
    Signed-off-by: Felipe <fdeodato11@gmail.com>

--- app/controllers/arquivos_controller.rb
+++ app/controllers/arquivos_controller.rb
@@ -5,15 +5,40 @@ class ArquivosController < ApplicationController
   end
 
   def new
+    @arquivo = Arquivo.new
   end
 
-  def create
-  end
-
+  # GET /arquivos/1/edit
   def edit
   end
 
+  # POST /arquivos
+  # POST /arquivos.json
+  def create
+    @arquivo = Arquivo.new(arquivo_params)
+    respond_to do |format|
+      if @arquivo.save
+        ligacao = UserArquivo.new
+        ligacao.arquivo_id = @arquivo.id
+        ligacao.user_id = current_user.id
+        ligacao.save! 
+        byebug
+        format.html { redirect_to arquivos_url, notice: 'Arquivo was successfully created.' }
+        format.json { render :idex, status: :created, location: @arquivo }
+      else
+        format.html { render :new }
+        format.json { render json: @arquivo.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def show
+  end
+
+  private 
+
+  def arquivo_params
+    params.require(:arquivo).permit(:nome)
   end
 
 end
---
+++ app/views/arquivos/_arquivo.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! arquivo, :nome, :created_at, :updated_at
+json.url arquivo_url(arquivo, format: :json)
